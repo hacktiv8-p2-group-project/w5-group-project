ee79111aab370f6375130349eaa3a1606d5fa0c1
CLorenzoH <christ.lorenzo.h@gmail.com>
2021-02-04 18:15:56 +0700

    christ 3:html

---
+++ client/index.js
@@ -0,0 +1,27 @@
+//@ts-check
+$(document).ready(() => {
+    auth()
+})
+
+const base_url = "http://localhost:3000/"
+
+function auth() {
+    if (!localStorage.getItem("access_token")) {
+        //
+    } else {
+        //
+    }
+}
+
+const cat_btn = document.getElementById("cat_btn")
+const cat_result = document.getElementById("cat_result")
+
+cat_btn.addEventListener("click", getRandomCat)
+
+function getRandomCat() {
+    fetch("https://aws.random.cat/meow")
+        .then((res) => res.json())
+        .then((data) => {
+            cat_result.innerHTML = `<img src=${data.file} alt="cat" />`
+        })
+}
