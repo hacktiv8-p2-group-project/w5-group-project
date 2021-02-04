30aeabeaf3b52c157fe26cd531bf2896f95c1141
CLorenzoH <christ.lorenzo.h@gmail.com>
2021-02-04 18:42:00 +0700

    christ 3: html fix

--- client/index.js
+++ client/index.js
@@ -13,15 +13,19 @@ function auth() {
     }
 }
 
-const cat_btn = document.getElementById("cat_btn")
-const cat_result = document.getElementById("cat_result")
+// const cat_btn = document.getElementById("cat_btn")
+// const cat_result = document.getElementById("cat_result")
 
-cat_btn.addEventListener("click", getRandomCat)
+// cat_btn.addEventListener("click", getRandomCat)
 
-function getRandomCat() {
-    fetch("https://aws.random.cat/meow")
-        .then((res) => res.json())
-        .then((data) => {
-            cat_result.innerHTML = `<img src=${data.file} alt="cat" />`
-        })
-}
+// function getRandomCat() {
+//     fetch("https://aws.random.cat/meow")
+//         .then((res) => res.json())
+//         .then((data) => {
+//             cat_result.innerHTML = `<img src=${data.file} alt="cat" />`
+//         })
+// }
+
+$("#cat_btn").click((e) => {
+    e.preventDefault()
+})
