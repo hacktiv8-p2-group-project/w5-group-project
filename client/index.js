e44b8512af32a4945adea4d305c29ebb1fa5ad87
CLorenzoH <christ.lorenzo.h@gmail.com>
2021-02-04 17:08:32 +0700

    christ 2: html test

---
+++ client/index.js
@@ -0,0 +1,14 @@
+//@ts-check
+$(document).ready(() => {
+    auth()
+})
+
+const base_url = "http://localhost:3000/"
+
+function auth() {
+    if (!localStorage.getItem("access_token")) {
+        //
+    } else {
+        //
+    }
+}
