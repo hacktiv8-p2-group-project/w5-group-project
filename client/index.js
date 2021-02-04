46196bcbef0be6d57635bf90df34ebb66f9fcdb5
Daniel Adhi Nugraha <danielanugr@yahoo.co.id>
2021-02-04 21:19:05 +0700

    google login

--- client/index.js
+++ client/index.js
@@ -1,17 +1,34 @@
-//@ts-check
 $(document).ready(() => {
-    auth()
+  auth()
 })
 
 const base_url = "http://localhost:3000/"
 
 function auth() {
-    if (!localStorage.getItem("access_token")) {
-        //
-    } else {
-        //
-    }
+  if (!localStorage.getItem("access_token")) {
+    //
+  } else {
+    //
+  }
 }
+
+function onSignIn(googleUser) {
+  var id_token = googleUser.getAuthResponse().id_token;
+  $.ajax({
+    url: base_url + `googleLogin`,
+    method: "POST",
+    data: {
+      google_token: id_token
+    }
+  })
+    .done(res => {
+      console.log(res);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+}
+
 
 // const cat_btn = document.getElementById("cat_btn")
 // const cat_result = document.getElementById("cat_result")
@@ -27,5 +44,5 @@ function auth() {
 // }
 
 $("#cat_btn").click((e) => {
-    e.preventDefault()
+  e.preventDefault()
 })
